53f09138e535c64f02c8fef2661a3caeb2fe8bd8
Arianna <arianna.cella@gmail.com>
2023-02-09 09:49:03 +0100

    va

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express= require("express");
 const app=express();
 const mysql= require("mysql");
 const dotenv = require("dotenv");
+const bodyParser = require('body-parser');
 const userRoute = require("./routes/user");
 dotenv.config();
 
@@ -27,6 +28,17 @@ app.use("/api/user", userRoute); //cosa fa use?
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
+
+app.get('/getfood', (req, res) => {
+  let queryString = 'SELECT * FROM cibo';
+  connection.query(queryString, function(err, result, fields) {
+  if (err) throw err;
+  for (let i in result) {
+    console.log('name: ', result[i].Nome);
+  }
+  res.send(result)
+  });
+  })
 
 
 app.listen(process.env.PORT || 3001, ()=>{
